d3d1b43e155972055f6b884039137e03ffff1853
Soland <francois.gruau@gmail.com>
2013-05-01 12:19:22 +0200

    beta version of the DCAT generator command (generate a DCAT file from the data in the Odalisk SQL database)

---
+++ src/OdaliskProject/Bundle/Command/DCATGenerateCommand.php
@@ -0,0 +1,221 @@
+<?php
+
+namespace OdaliskProject\Bundle\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use OdaliskProject\Bundle\Entity\Portal;
+
+use OdaliskProject\Bundle\Entity\Dataset;
+
+/**
+ * A command that will generate a DCAT file using data from Odalisk SQL database
+ */
+class DCATGenerateCommand extends BaseCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('odalisk:dcat:generate')
+            ->setDescription('Generate a DCAT file for a portal already crawled')
+            ->addArgument('platform', InputArgument::OPTIONAL,
+                'Which platform do you want to generate a DCAT file for ?'
+            )
+            ->addOption('list', null, InputOption::VALUE_NONE,
+                'If set, the task will display available platforms names rather than generate a DCAT file for them'
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+       $start = time();
+        // Store the container so that we have an easy shortcut
+        $container = $this->getContainer();
+        // Get the configuration value from config/app.yml : which platforms are enabled?
+        $platformServices = $container->getParameter('config.enabled_portals');
+        // Get the data directory
+        $dataPath = $container->getParameter('config.file_dumper.data_path');
+        // Entity repository for datasets_crawls & entity manager
+        $em = $this->getEntityManager();
+        //$em->getConnection()->getConfiguration()->setSQLLogger(new \Doctrine\DBAL\Logging\EchoSQLLogger());
+        
+        $portalRepo = $this->getEntityRepository('OdaliskProject\Bundle\Entity\Portal');
+        
+        // Initialize some arrrays
+        $platforms = array();
+
+        // If the --list switch was used, just list the enabled platforms names
+        if ($input->getOption('list')) {
+            foreach ($platformServices as $platform) {
+                $output->writeln('<info>' . $platform . '</info>');
+            }
+        } else {
+            // If we get an argument, replace the platformServices array with one containing just that plaform
+            if ($platform = $input->getArgument('platform')) {
+                 $platformServices = array($platform);
+            }
+
+            // Iterate on the enabled platforms to retrieve the actual object
+            foreach ($platformServices as $platform) {
+                // Store the platform object
+                $platforms[$platform] = $container->get($platform);
+            }
+            
+            // Empty the database beforehand
+            foreach($platforms as $name => $platform) {
+            }
+
+            // Process each platform :
+            foreach ($platforms as $name => $platform) {
+                error_log('[DCATGeneration] Processing started for ' . $platform->getName());
+                // Load the portal object from the database
+                $portal = $platform->loadPortal();                
+                // Cache the platform path
+                $platformPath = $dataPath . $name . '/';
+
+
+                $this->generatePortalInfo($portal);
+
+                $datasets = $portal->getDatasets();
+    
+                foreach ($datasets as $dataset) {
+                    $this->generateDatasetInfo($portal, $dataset);
+                }
+        $filename = $portal->getName() . ".rdf";
+
+        if (!$handle = fopen($filename, 'a')) {
+            error_log("Impossible d'ouvrir le fichier" . $filename);
+              exit;
+        }
+                    fwrite($handle,"\t</dcat:Catalog>\n");
+                fwrite($handle,"</rdf:RDF>\n");
+        fclose($handle);
+
+                }
+        }
+        $end = time();
+        error_log('[DCATGeneration] Processing ended after ' . ($end - $start) . ' seconds');
+    }
+    
+    protected function generatePortalInfo(Portal $portal){
+        $filename = $portal->getName() . ".rdf";
+        
+        // Assurons nous que le fichier est accessible en écriture
+
+        // Dans notre exemple, nous ouvrons le fichier $filename en mode d'ajout
+        // Le pointeur de fichier est placé à la fin du fichier
+        // c'est là que $somecontent sera placé
+        if (!$handle = fopen($filename, 'a')) {
+            error_log("Impossible d'ouvrir le fichier" . $filename);
+              exit;
+        }
+
+       // Ecrivons quelque chose dans notre fichier.
+           // Select language based on location
+            $lang = array(  'European Union' => "en", 
+                            'France'         => "fr",
+                         );
+
+            fwrite($handle,"<?xml version=\"1.0\" encoding=\"utf-8\"?>\n");
+            fwrite($handle,"<rdf:RDF xmlns:foaf=\"http://xmlns.com/foaf/0.1/\"\n");
+            fwrite($handle,"\t\t xmlns:skos=\"http://www.w3.org/2004/02/skos/core#\"\n");
+            fwrite($handle,"\t\t xmlns:dcat=\"http://www.w3.org/ns/dcat#\"\n");
+            fwrite($handle,"\t\t xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"\n");
+            fwrite($handle,"\t\t xmlns:rdfs=\"http://www.w3.org/2000/01/rdf-schema#\"\n");
+            fwrite($handle,"\t\t xmlns:dct=\"http://purl.org/dc/terms/\"\n");
+            fwrite($handle,"\t\t xmlns:dctypes=\"http://purl.org/dc/dcmitype/\"\n");
+            fwrite($handle,"\t\t xmlns:owl=\"http://www.w3.org/2002/07/owl#\">\n");
+
+                fwrite($handle,"\t<dcat:Catalog rdf:about=\"" . $portal->getUrl() . "\">\n");
+                    fwrite($handle,"\t\t<dct:title xml:lang=\"" . $lang[$portal->getCountry()] . "\">" .$portal->getName() . "</dct:title>\n");
+                    fwrite($handle,"\t\t<dct:description xml:lang=\"" . $lang[$portal->getCountry()] . "\"></dct:description>\n");
+                    fwrite($handle,"\t\t<foaf:homepage rdf:resource=\"" . $portal->getUrl() . "\"/>\n");
+                    fwrite($handle,"\t\t<dct:spatial>\n");
+                        fwrite($handle,"\t\t\t<dct:Location>\n");
+                            fwrite($handle,"\t\t\t\t<rdfs:label xml:lang=\"" . $lang[$portal->getCountry()] . "\">" . $portal->getCountry() . "</rdfs:label>\n");
+                            fwrite($handle,"\t\t\t\t<rdfs:seeAlso rdf:resource=\"\"/>\n");
+                        fwrite($handle,"\t\t\t</dct:Location>\n");
+                    fwrite($handle,"\t\t</dct:spatial>\n");
+                    fwrite($handle,"\t\t<dct:license rdf:resource=\"\"/>\n");
+                    fwrite($handle,"\t\t<dct:publisher>\n");
+                        fwrite($handle,"\t\t\t<foaf:Organization>\n");
+                            fwrite($handle,"\t\t\t\t<dct:title xml:lang=\"" . $lang[$portal->getCountry()] . "\">" . $portal->getEntity() . "</dct:title>\n");
+                            fwrite($handle,"\t\t\t\t<foaf:homepage rdf:resource=\"" . $portal->getUrl() . "\"/>\n");
+                        fwrite($handle,"\t\t\t</foaf:Organization>\n");
+                    fwrite($handle,"\t\t</dct:publisher>\n");
+
+    
+        
+
+        error_log("réussi");
+        fclose($handle);
+
+    }
+
+    protected function generateDatasetInfo(Portal $portal, Dataset $dataset){
+        $filename = $portal->getName() . ".rdf";
+        
+        // Assurons nous que le fichier est accessible en écriture
+
+        // Dans notre exemple, nous ouvrons le fichier $filename en mode d'ajout
+        // Le pointeur de fichier est placé à la fin du fichier
+        // c'est là que $somecontent sera placé
+        if (!$handle = fopen($filename, 'a')) {
+            error_log("Impossible d'ouvrir le fichier" . $filename);
+              exit;
+        }
+
+       // Ecrivons quelque chose dans notre fichier.
+           // Select language based on location
+            $lang = array(  'European Union' => "en", 
+                            'France'         => "fr",
+                         );
+
+            fwrite($handle,"\t\t<dcat:dataset>\n");
+            fwrite($handle,"\t\t\t<dcat:Dataset rdf:about=\"" . $dataset->getUrl() . "\">\n"); 
+            fwrite($handle,"\t\t\t\t<dct:issued rdf:datatype=\"http://www.w3.org/2001/XMLSchema#date\">" . $dataset->getReleasedOn() . "</dct:issued>\n");
+            fwrite($handle,"\t\t\t\t<dct:modified rdf:datatype=\"http://www.w3.org/2001/XMLSchema#date\">" . $dataset->getLastUpdatedOn() . "</dct:modified>\n");
+            fwrite($handle,"\t\t\t\t<dct:creator>" . $dataset->getProvider() . "</dct:creator>\n");
+            fwrite($handle,"\t\t\t\t<dct:description xml:lang=\"" . $lang['France'] . "\">" . $dataset->getSummary() . "</dct:description>\n");
+            fwrite($handle,"\t\t\t\t<dct:license rdf:resource=\"\"/>\n");
+
+            fwrite($handle,"\t\t\t\t<dcat:keyword xml:lang=\"" . $lang['France'] . "\"></dcat:keyword>\n");
+            fwrite($handle,"\t\t\t\t<dcat:distribution>\n");
+               fwrite($handle,"\t\t\t\t\t<dcat:Download>\n");
+                  fwrite($handle,"\t\t\t\t\t\t<dcat:accessURL>" . $dataset->getUrl() . "</dcat:accessURL>\n");
+
+
+
+                  fwrite($handle,"\t\t\t\t\t\t<dct:format>\n");
+
+                  $formats = $dataset->getFormats();
+                  foreach($formats as $format){
+                    fwrite($handle,"\t\t\t\t\t\t\t<dct:IMT>\n");
+                       // fwrite($handle,"\t\t\t\t\t\t\t\t<rdf:value></rdf:value>\n");
+                        fwrite($handle,"\t\t\t\t\t\t\t\t<rdfs:label>" . $format->getFormat() . "</rdfs:label>\n");
+                    fwrite($handle,"\t\t\t\t\t\t\t</dct:IMT>\n");        
+
+                  }
+
+
+                  fwrite($handle,"\t\t\t\t\t\t</dct:format>\n");
+
+                  fwrite($handle,"\t\t\t\t\t\t<dct:modified rdf:datatype=\"http://www.w3.org/2001/XMLSchema#date\">" . $dataset->getLastUpdatedOn() . "</dct:modified>\n");
+               fwrite($handle,"\t\t\t\t\t</dcat:Download>\n");
+            fwrite($handle,"\t\t\t\t</dcat:distribution>\n");
+            fwrite($handle,"\t\t\t\t<dct:publisher>\n");
+               fwrite($handle,"\t\t\t\t\t<foaf:Organization>\n");
+                  fwrite($handle,"\t\t\t\t\t\t<dct:title xml:lang=\"" . $lang['France'] . "\">" . $dataset->getOwner() . "</dct:title>\n");
+                  fwrite($handle,"\t\t\t\t\t\t<foaf:homepage rdf:resource=\"" . $portal->getUrl() . "\"/>\n");
+               fwrite($handle,"\t\t\t\t\t</foaf:Organization>\n");
+            fwrite($handle,"\t\t\t\t</dct:publisher>\n");
+         fwrite($handle,"\t\t\t</dcat:Dataset>\n");
+      fwrite($handle,"\t\t</dcat:dataset>\n");
+
+      fclose($handle);
+
+    }    
+}
