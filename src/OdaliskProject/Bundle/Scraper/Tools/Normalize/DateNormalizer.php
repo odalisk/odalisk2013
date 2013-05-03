7091499ef0417785ac182f87660801d39270849b
Soland <francois.gruau@gmail.com>
2013-05-03 20:18:10 +0200

    -  inclused french dates normalization

--- src/OdaliskProject/Bundle/Scraper/Tools/Normalize/DateNormalizer.php
+++ src/OdaliskProject/Bundle/Scraper/Tools/Normalize/DateNormalizer.php
@@ -5,6 +5,50 @@ namespace OdaliskProject\Bundle\Scraper\Tools\Normalize;
 class DateNormalizer
 {
     /**
+     * Match some regex to translate months from word to number ie: jan => 01
+     *
+     * @var string
+     */
+    protected $translateMonths = array(
+        '/(.+)?(janvier|jan|january)(-|\s.+)/i' =>  array(
+            '01',
+        ),
+        '/(.+)?(février|fév|february|feb)(-|\s.+)/i' =>  array(
+            '02',
+        ),
+        '/(.+)?(mars|mar|march)(-|\s.+)/i' =>  array(
+            '03',
+        ),
+        '/(.+)?(avril|avr|april|apr)(-|\s.+)/i' =>  array(
+            '04',
+        ),
+        '/(.+)?(mai|may)(-|\s.+)/i' =>  array(
+            '05',
+        ),
+        '/(.+)?(juin|june|jun)(-|\s.+)/i' =>  array(
+            '06',
+        ),
+        '/(.+)?(juillet|july|jul)(-|\s.+)/i' =>  array(
+            '07',
+        ),
+        '/(.+)?(aout|août|aou|august|aug)(-|\s.+)/i' =>  array(
+            '08',
+        ),
+        '/(.+)?(septembre|sept|sep|september)(-|\s.+)/i' =>  array(
+            '09',
+        ),
+        '/(.+)?(octobre|oct|october)(-|\s.+)/i' =>  array(
+            '10',
+        ),
+        '/(.+)?(novembre|nov|november)(-|\s.+)/i' =>  array(
+            '11',
+        ),
+        '/(.+)?(décembre|dec|december)(-|\s.+)/i' =>  array(
+            '12',
+        ),
+    );
+
+    /**
      * Match some regex to known date formats. Order is IMPORTANT!
      *
      * @var string
@@ -34,22 +78,6 @@ class DateNormalizer
             '!d*m*y H:i',
             '!d*m*y H:i:s',
         ),
-        '/^(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) ([0-9]{1,2}[^0-9]+)?[0-9]{4}$/' => array(
-            '!M*Y',
-            '!M*d?*Y',
-        ),
-        '/^([0-9]{1,2}.)?(?:January|February|March|April|May|June|July|August|September|October|November|December).[0-9]{4}$/' => array(
-            '!F?Y',
-            '!d?F?Y',
-        ),
-        '/^([0-9]{1,2}.)?(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec).[0-9]{2}$/' => array(
-            '!M?y',
-            '!d?M?y',
-        ),
-        '/^([0-9]{1,2}.)?(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec).[0-9]{4}?$/' => array(
-            '!M?Y',
-            '!d?M?Y',
-        ),
     );
 
     /**
@@ -57,7 +85,7 @@ class DateNormalizer
      *
      * @var array $emptyDates
      */
-    protected $emptyDates = array('N/A', 'n/a', 'TBC', 'not known', '');
+    protected $emptyDates = array('/', 'TBC', 'not known', '');
 
     /**
      * The date type fields we need to process so we can transform them into DateTime objects
@@ -73,10 +101,24 @@ class DateNormalizer
 
     public function normalize(array &$data)
     {
+        $i = 0;
         // We transform dates strings in datetime.
         foreach ($this->dateFields as $field) {
             if (array_key_exists($field, $data)) {
                 $date = $data[$field];
+
+                $dateO = $date;
+
+                foreach($this->translateMonths as $tM => $month){
+
+                    $date = preg_replace($tM, '${1}'. $month[count($month)-1] . '$3', $date);
+                }
+                $date = preg_replace('#[A-Za-zÁÀÂÄÉÈÊËÍÌÎÏÓÒÔÖÚÙÛÜáàâäéèêëíìîïóòôöúùûüÇç&\(\)]+#', '', $date);               
+                $date = trim($date);
+
+               // error_log($date);
+
+
                 // Try to match the date against something we know
                 foreach ($this->correctDates as $regex => $formats) {
                     // Check if we have a match
@@ -101,7 +143,7 @@ class DateNormalizer
                     $data[$field] = null;
                 } else {
                     // Not something we recognize
-                    error_log('[' . date('d-M-Y H:i:s') . '] [Unknown date format] ' . $date . "\n", 3, $this->log);
+                    error_log('[' . date('d-M-Y H:i:s') . '] [Unknown date format] ' .$dateO. ' => ' . $date . "\n", 3, $this->log);
                     $data[$field] = $date;
                 }
                 $date = null;
