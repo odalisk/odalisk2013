ddeb7c22f916f03463004d8613ec8763cbdd37c1
PaulFrancois <paulfrancois.jeau@gmail.com>
2013-05-05 14:23:38 +0200

    New controller

---
+++ src/OdaliskProject/Bundle/Controller/RdfController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace OdaliskProject\Bundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class RdfController extends Controller
+{
+    public function dcatAction($idMongo)
+    {
+        //We look for the mongdb document manager and for the file we want to send
+        $mana = $this->container->get('doctrine.odm.mongodb.document_manager')->getRepository('OdaliskProject\Bundle\Document\DcatDataset');
+        $file = $mana->find($idMongo);
+
+        //And then goes the response construction
+        $response = new Response();
+        $response->setContent(json_decode($file->getFile()->getBytes(),true));
+        $response->headers->set('Content-type', 'application/rdf');
+        $filename = $idMongo.".rdf";
+        $response->headers->set('Content-Disposition: attachment;', 'filename='.$filename);
+
+        return $response;
+
+    }
+}
